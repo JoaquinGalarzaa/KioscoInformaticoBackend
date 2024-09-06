292c78ac3485e38656e78c2f4d5e571318c7e6fe
JoaquinGalarzaa <joaa.galarza@gmail.com>
2024-09-06 15:38:15 -0300

    Agregar archivos de proyecto.

---
+++ Models/Compra.cs
@@ -0,0 +1,24 @@
+﻿using KioscoInformaticoBackend.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace KioscoInformaticoBackend.Models;
+
+public partial class Compra
+{
+    public int Id { get; set; }
+
+    public FormaDePagoEnum  FormaDePago { get; set; }
+
+    public int Iva { get; set; }
+
+    public int Total { get; set; }
+
+    public DateTime Fecha { get; set; }
+
+    public int? ProveedorId { get; set; }
+
+    public virtual ICollection<Detallescompra> Detallescompras { get; set; } = new List<Detallescompra>();
+
+    public virtual Proveedor? Proveedor { get; set; }
+}
---
+++ Models/Venta.cs
@@ -0,0 +1,24 @@
+﻿using KioscoInformaticoBackend.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace KioscoInformaticoBackend.Models;
+
+public partial class Venta
+{
+    public int Id { get; set; }
+
+    public FormaDePagoEnum FormaPago { get; set; }
+
+    public int ClienteId { get; set; }
+
+    public DateTime Fecha { get; set; }
+
+    public decimal Iva { get; set; }
+
+    public decimal Total { get; set; }
+
+    public virtual Cliente Cliente { get; set; } = null!;
+
+    public virtual ICollection<Detallesventa> Detallesventa { get; set; } = new List<Detallesventa>();
+}
